38dea2028a5a88640ec12350ce9718e004de2efd
Connor <cstreng95@gmail.com>
2020-03-06 16:26:58 +0100

    game can be played with the mouse... there are still no images for rock paper or scissors options... round count and win/loss/tie count are updated upon the deciding of the round... there is still no UI to start the game or to show what the computer chose

--- rps.js
+++ rps.js
@@ -1,116 +1,103 @@
 var options = Array('ROCK' , 'PAPER' , 'SCISSORS');
-let computerSelection;
-let playerSelection;
+document.getElementById("Rock").addEventListener("click", playerRock);
+document.getElementById("Paper").addEventListener('click' , playerPaper);
+document.getElementById("Scissors").addEventListener('click' , playerScissors);
+
 let winCount = 0;
 let lossCount = 0;
 let tieCount = 0;
 let roundCount = 1;
 
-document.getElementById("Rock").addEventListener("click", rockwasclicked);
-document.getElementById("Paper").addEventListener('click' , paperwasclicked);
-document.getElementById("Scissors").addEventListener('click' , scissorswasclicked);
+let wins = document.getElementById("wins");
+wins.innerHTML = winCount;
+let losses = document.getElementById("losses");
+losses.innerHTML = lossCount;
+let ties = document.getElementById("ties");
+ties.innerHTML = tieCount;
+let round = document.getElementById("roundnumber");
+round.innerHTML = roundCount;
+
 
 function computerPlay(){ //this randomizes the computers selection
-    computerSelection = options[Math.floor(Math.random() *options.length)];
+    let = computerSelection = options[Math.floor(Math.random() *options.length)];
     return computerSelection;
 }
-
-function rockwasclicked(){ //this is run if rock is clicked
-    console.log('rock');
-    playerRock();
-}
-
-function paperwasclicked(){ //this is run if paper is clicked
-    console.log('paper');
-    playerPaper();
-}
-
-function scissorswasclicked(){ //this is run if scissors is clicked
-    console.log('scissors');
-    playerScissors();
-}
-
-function playRound(playerSelection){ //this function takes the players selection as input and decides which function to run based on their selection
-    let computerSelection = computerPlay();
-    playerSelection = playerSelection.toUpperCase();
-    console.log(playerSelection + ' VS ' + computerSelection);
-    if (playerSelection === 'ROCK'){
-        playerRock();
-    } else if (playerSelection === 'PAPER'){
-        playerPaper();
-    } else if (playerSelection == 'SCISSORS'){
-        playerScissors();
-    } else {
-          console.log('Something is wrong here!');
-    }
-   return playerSelection;
-}
-
 
 function playerRock(){ //this plays upon rock being clicked
     let x = computerPlay();
    if (x == 'ROCK'){
         console.log('TIE!');
-        return ++tieCount;
+        tieCount++;
+        updateTieCount(tieCount);
    } else if (x == 'PAPER'){
         console.log('You Lose!  PAPER beats ROCK!');
-        return ++lossCount;
+        lossCount++;
+        updateLossCount(lossCount);
    } else if (x == 'SCISSORS'){
         console.log('You Win! ROCK beats SCISSORS!');
-        return ++winCount;
+        winCount++;
+        updateWinCount(winCount);
    } else {
         console.log('Something is wrong here.');
     }
+    ++roundCount
+    updateRoundCount(roundCount);
 }
-
 
 function playerPaper(){ // this plays upon paper being clicked
     let x = computerPlay();
     if (x == 'PAPER'){
         console.log('TIE!');
-        return ++tieCount;
+        ++tieCount;
+        updateTieCount(tieCount);
     } else if (x == 'ROCK'){
         console.log('You Win! PAPER beats ROCK!');
-        return ++winCount;
+        ++winCount;
+        updateWinCount(winCount);
     } else if (x == 'SCISSORS') {
         console.log('You Lose! SCISSORS beats PAPER!');
-        return ++lossCount;
+        ++lossCount;
+        updateLossCount(lossCount);
     } else {
         console.log('Something is wrong here.');
     }
+    ++roundCount
+    updateRoundCount(roundCount);
 }
 
-
-function playerScissors(){ //if the player selects scissors then run this function
+function playerScissors(){ //this plays upon scissors being clicked
     let x = computerPlay();
     if (x == 'SCISSORS'){
         console.log('TIE!');
-        return ++tieCount;
+        ++tieCount;
+        updateTieCount(tieCount);
     } else if (x == 'PAPER'){
         console.log('You win! SCISSORS beats PAPER!');
-        return ++winCount;
+        ++winCount;
+        updateWinCount(winCount);
     } else if (x == 'ROCK'){
         console.log('You Lose! ROCK beats SCISSORS!');
-        return ++lossCount;
+        ++lossCount;
+        updateLossCount(lossCount);
     } else {
         console.log('Something is wrong here.');
     }
+    ++roundCount
+    updateRoundCount(roundCount);
 }
 
-function game(){
-   do{
-    let playerSelection = window.prompt('ROUND ' + roundCount + ': What is your selection?');
-    playRound(playerSelection);
-    console.log('Wins: ' + winCount + ' Losses: ' + lossCount + ' Ties: ' + tieCount);
-    ++roundCount;
-   }
-   while ((winCount + lossCount) < 5);
-   //return winner//
-   if (winCount > lossCount){
-       console.log('You are the WINNER!');
-   } else if (lossCount > winCount){
-       console.log('You are the LOSER.');
-   } else {
-       console.log('Something might be wrong here.');
-   }
+function updateRoundCount(){ //this updates the round count variable and reassigns the div on the page to the current round count
+    round.innerHTML = roundCount;
+}
+
+function updateTieCount(){ //this updates the tiecount variable and reassigns the div on the page
+    ties.innerHTML = tieCount;
+}
+
+function updateLossCount(){ //this updates the losscount variable and reassigns the div on the page
+    losses.innerHTML = lossCount;
+}
+
+function updateWinCount(){ //this updates the wincount variable and reassigns the div on the page
+    wins.innerHTML = winCount;
 }
