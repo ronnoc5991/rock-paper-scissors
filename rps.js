0d08a0ae62d0b8f9bed7acf27669d5c276f238ee
Connor <cstreng95@gmail.com>
2020-03-09 11:30:37 +0100

    functional up to five rounds... still needs work on UI

--- rps.js
+++ rps.js
@@ -1,27 +1,53 @@
 var options = Array('ROCK' , 'PAPER' , 'SCISSORS');
-document.getElementById("Rock").addEventListener("click", playerRock);
-document.getElementById("Paper").addEventListener('click' , playerPaper);
-document.getElementById("Scissors").addEventListener('click' , playerScissors);
-document.getElementById("play").addEventListener('click', playclicked);
-
-function playclicked(){
-    console.log('play');
-}
-
+const playbutton = document.getElementById("play");
+const rock = document.getElementById("Rock");
+const paper = document.getElementById("Paper");
+const scissors = document.getElementById("Scissors");
+let wins = document.getElementById("wins");
+let losses = document.getElementById("losses");
+let ties = document.getElementById("ties");
+let round = document.getElementById("roundnumber");
 let winCount = 0;
 let lossCount = 0;
 let tieCount = 0;
 let roundCount = 1;
+let score = 0;
 
-let wins = document.getElementById("wins");
-wins.innerHTML = winCount;
-let losses = document.getElementById("losses");
-losses.innerHTML = lossCount;
-let ties = document.getElementById("ties");
-ties.innerHTML = tieCount;
-let round = document.getElementById("roundnumber");
-round.innerHTML = roundCount;
+playbutton.addEventListener('click' , startGame);
 
+function startGame(){ //initializes the event listeners and removes event listener from the playbutton
+    wins.innerHTML = winCount;
+    losses.innerHTML = lossCount;
+    ties.innerHTML = tieCount;
+    round.innerHTML = roundCount;
+    rock.addEventListener("click", playerRock);
+    paper.addEventListener('click' , playerPaper);
+    scissors.addEventListener('click' , playerScissors);
+    rock.addEventListener("click", playRound);
+    paper.addEventListener('click' , playRound);
+    scissors.addEventListener('click' , playRound);
+    playbutton.removeEventListener('click' , startGame);
+}
+
+
+function playRound(){ //keeps track of round... after five rounds removes the event listeners from buttons
+    if (roundCount == 6){ // when score = 5, remove the listeners
+        rock.removeEventListener("click" , playerRock);
+        paper.removeEventListener("click" , playerPaper);
+        scissors.removeEventListener("click" , playerScissors);
+        rock.removeEventListener("click" , playRound);
+        paper.removeEventListener("click" , playRound);
+        scissors.removeEventListener("click" , playRound);
+        if (winCount > lossCount){
+            console.log('You are ze winner!');//calculate winner/loser
+        } else if (lossCount > winCount){
+            console.log('You are ze loser!');
+        } else console.log("I think this is a tie")
+}
+}
+//I need something that evaluates the score and removes the event listeners as well as declares a winner when the score is equal to five
+
+//else = run the event listeners
 
 function computerPlay(){ //this randomizes the computers selection
     let = computerSelection = options[Math.floor(Math.random() *options.length)];
@@ -92,7 +118,9 @@ function playerScissors(){ //this plays upon scissors being clicked
 }
 
 function updateRoundCount(){ //this updates the round count variable and reassigns the div on the page to the current round count
-    round.innerHTML = roundCount;
+    if (roundCount <= 5){
+        round.innerHTML = roundCount;
+    } else round.innerHTML = 5;
 }
 
 function updateTieCount(){ //this updates the tiecount variable and reassigns the div on the page
@@ -105,4 +133,5 @@ function updateLossCount(){ //this updates the losscount variable and reassigns 
 
 function updateWinCount(){ //this updates the wincount variable and reassigns the div on the page
     wins.innerHTML = winCount;
+    return winCount;
 }
